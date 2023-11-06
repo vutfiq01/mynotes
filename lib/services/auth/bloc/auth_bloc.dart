2eb1bc3d190a6828d554f4158fc567f5b8b1fe58
vutfiq01 <rafiq.du.math@gmail.com>
2023-11-06 22:46:42 +0600

    loginview exceptions handled using bloc

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -9,7 +9,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       await provider.initialize();
       final user = provider.currentUser;
       if (user == null) {
-        emit(const AuthStateLoggedOut());
+        emit(const AuthStateLoggedOut(null));
       } else if (!user.isEmailVerified) {
         emit(const AuthStateNeedsVerification());
       } else {
@@ -18,7 +18,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     });
 
     on<AuthEventLogin>((event, emit) async {
-      emit(const AuthStateLoading());
       final email = event.email;
       final password = event.password;
       try {
@@ -28,16 +27,16 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         );
         emit(AuthStateLoggedIn(user));
       } on Exception catch (e) {
-        emit(AuthStateLoginFailure(e));
+        emit(AuthStateLoggedOut(e));
       }
     });
     on<AuthEventLogout>((event, emit) async {
       try {
         emit(const AuthStateLoading());
         await provider.logOut();
-        emit(const AuthStateLoggedOut());
+        emit(const AuthStateLoggedOut(null));
       } on Exception catch (e) {
-        emit(AuthStateLoginFailure(e));
+        emit(AuthStateLogoutFailure(e));
       }
     });
   }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,6 +3,7 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -58,33 +59,37 @@ class _LoginViewState extends State<LoginView> {
               hintText: 'Password',
             ),
           ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-
-              try {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is InvalidLoginCredentialsAuthException) {
+                  if (mounted) {
+                    await showErrorDialog(
+                      context,
+                      'Either email or password or both incorrect.',
+                    );
+                  }
+                } else if (state.exception is GenericAuthException) {
+                  if (mounted) {
+                    await showErrorDialog(
+                      context,
+                      'Authentication error.',
+                    );
+                  }
+                }
+              }
+            },
+            child: TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
                 context.read<AuthBloc>().add(AuthEventLogin(
                       email,
                       password,
                     ));
-              } on InvalidLoginCredentialsAuthException {
-                if (mounted) {
-                  await showErrorDialog(
-                    context,
-                    'Either email or password or both incorrect.',
-                  );
-                }
-              } on GenericAuthException {
-                if (mounted) {
-                  await showErrorDialog(
-                    context,
-                    'Authentication error.',
-                  );
-                }
-              }
-            },
-            child: const Text('Login'),
+              },
+              child: const Text('Login'),
+            ),
           ),
           TextButton(
             onPressed: () {
